c012a929f7c6c8b67b427f299f52d82d150290d9
Christian Berg <berg.christian@gmail.com>
2015-09-26 07:45:43 +0200

    Use a fixture to setup the system for testing

--- test/speicherstadt/acceptance/chunks.clj
+++ test/speicherstadt/acceptance/chunks.clj
@@ -3,11 +3,20 @@
             [com.stuartsierra.component :as component]
             [clojure.test :refer :all]))
 
+(def ^:dynamic *system* nil)
+
+(defn setup-system [f]
+  (binding [*system* (component/start
+                      (system/new-system {:http {:port 3333}}))]
+    (f)
+    (component/stop *system*)))
+
+(use-fixtures :each setup-system)
+
 (deftest chunk-acceptance
-  (let [sys (component/start (system/new-system {:http {:port 3333}}))]
-    (let [handler (-> sys :app :handler)]
-      (is (= (-> {:uri "/chunks/dead-beef"
-                  :request-method :put}
-                 handler
-                 :status)
-             204)))))
+  (let [handler (-> *system* :app :handler)]
+    (is (= (-> {:uri "/chunks/dead-beef"
+                :request-method :put}
+               handler
+               :status)
+           204))))
