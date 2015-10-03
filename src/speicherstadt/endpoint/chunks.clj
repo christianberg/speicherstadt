5de673501d91635bdea1690cbddc86ebce63e3e9
Christian Berg <berg.christian@gmail.com>
2015-10-03 16:00:24 +0200

    Use ring.util functions to build response map
    [#104789850]

--- src/speicherstadt/endpoint/chunks.clj
+++ src/speicherstadt/endpoint/chunks.clj
@@ -1,17 +1,17 @@
 (ns speicherstadt.endpoint.chunks
   (:require [compojure.core :refer :all]
+            [ring.util.response :refer :all]
             [speicherstadt.component.chunk-storage :as storage]))
 
 (defn chunks-endpoint [{:keys [store]}]
   (routes
    (context "/chunks" []
             (GET "/" [] (fn [_]
-                          {:status 200
-                           :body (str (vec (storage/list-all store)))}))
+                          (response (str (vec (storage/list-all store))))))
             (GET "/:id" [id] (fn [_]
                                (if-let [value (storage/retrieve store id)]
-                                 {:status 200 :body value}
-                                 {:status 404})))
+                                 (response value)
+                                 (not-found "Chunk not found"))))
             (PUT "/:id" [id] (fn [request]
                                (storage/store store id (slurp (:body request)))
-                               {:status 204})))))
+                               (status {} 204))))))
