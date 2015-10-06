b9a16443be5c8dd0db022a43ceb72a1be7384bb2
Christian Berg <berg.christian@gmail.com>
2015-10-06 11:32:07 +0200

    Add more tests for Chunk listing
    [finishes #104789850]

--- test/speicherstadt/acceptance/chunks.clj
+++ test/speicherstadt/acceptance/chunks.clj
@@ -1,6 +1,7 @@
 (ns speicherstadt.acceptance.chunks
   (:require [speicherstadt.system :as system]
             [com.stuartsierra.component :as component]
+            [cheshire.core :as json]
             [clojure.test :refer :all]))
 
 (def ^:dynamic *system* nil)
@@ -15,6 +16,13 @@
 
 (deftest chunk-acceptance
   (let [handler (-> *system* :app :handler)]
+    (testing "List of chunks is empty before uploading any chunks"
+      (let [response (handler {:uri "/chunks"
+                               :request-method :get})]
+        (is (= (:status response) 200))
+        (is (= (get-in response [:headers "Content-Type"])
+               "application/json"))
+        (is (= (json/parse-string (:body response) true) []))))
     (testing "PUT a new chunk"
       (is (= (-> {:uri "/chunks/dead-beef"
                   :request-method :put
@@ -36,10 +44,24 @@
                  handler
                  :status)
              404)))
-    (testing "GET a list of all chunks"
+    (testing "GET a list of one chunk"
       (let [response (handler {:uri "/chunks"
                                :request-method :get})]
         (is (= (:status response) 200))
         (is (= (get-in response [:headers "Content-Type"])
                "application/json"))
-        (is (= (:body response) "[\"dead-beef\"]"))))))
+        (is (= (json/parse-string (:body response) true)
+               ["dead-beef"]))))
+    (testing "GET a list of two chunks"
+      (handler {:uri "/chunks/abc"
+                :request-method :put
+                :headers {"Content-Type" "application/octet-stream"}
+                :body (java.io.StringReader. "Hello Foo")})
+      (let [response (handler {:uri "/chunks"
+                               :request-method :get})]
+        (is (= (:status response) 200))
+        (is (= (get-in response [:headers "Content-Type"])
+               "application/json"))
+        (is (= (json/parse-string (:body response) true)
+               ["abc"
+                "dead-beef"]))))))
