26e20b587a67ceeaadd348c0a030aba74ba4cbe4
Christian Berg <berg.christian@gmail.com>
2015-12-31 22:30:19 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,17 +3,17 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.stuartsierra/component "0.2.3"]
+                 [com.stuartsierra/component "0.3.1"]
                  [compojure "1.4.0"]
-                 [duct "0.3.0"]
+                 [duct "0.5.6"]
                  [environ "1.0.0"]
                  [meta-merge "0.1.1"]
                  [me.raynes/fs "1.4.6"]
                  [ring "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
-                 [ring-jetty-component "0.2.2"]
+                 [ring-jetty-component "0.3.0"]
                  [ring/ring-json "0.4.0"]]
-  :plugins [[lein-environ "1.0.0"]
+  :plugins [[lein-environ "1.0.1"]
             [lein-gen "0.2.2"]]
   :generators [[duct/generators "0.3.0"]]
   :duct {:ns-prefix speicherstadt}
@@ -29,8 +29,8 @@
    :profiles/test {}
    :project/dev   {:source-paths ["dev"]
                    :repl-options {:init-ns user}
-                   :dependencies [[reloaded.repl "0.1.0"]
+                   :dependencies [[reloaded.repl "0.2.1"]
                                   [org.clojure/tools.namespace "0.2.11"]
-                                  [kerodon "0.6.1"]]
+                                  [kerodon "0.7.0"]]
                    :env {:port 3000}}
    :project/test  {}})
