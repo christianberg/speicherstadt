ad6506e6844be924c469d1602ea3b13d69257d3f
Christian Berg <berg.christian@gmail.com>
2016-03-13 00:01:08 +0100

    Include context in request path for metrics
    If compojure routes are defined within a context, the prometheus-clj
    middleware would only record the matched route path without the context.
    With this change, the context is prepended to the path.

--- src/speicherstadt/middleware/metrics.clj
+++ src/speicherstadt/middleware/metrics.clj
@@ -1,9 +1,21 @@
 (ns speicherstadt.middleware.metrics
   (:require [prometheus.core :as prometheus]
-            [speicherstadt.component.metrics-store :as store]))
+            [speicherstadt.component.metrics-store :as store])
+  (:import [clojure.lang IObj]))
 
 (defn wrap-metrics [handler app-name metrics-store]
   (store/middleware metrics-store handler app-name))
+
+(defn with-path
+  "Adds the matched compojure route as the :path response metadata attribute"
+  [request response]
+  (if-let [route (last (:compojure/route request))]
+    (if (instance? IObj response)
+      (let [response-meta (or (meta response) {})
+            context (or (:context request) "")]
+        (with-meta response (assoc response-meta :path (str context route))))
+      response)
+    response))
 
 (defn wrap-path [handler]
   (fn [request]
@@ -12,4 +24,4 @@
                     (fn [hndlr]
                       (fn [req]
                         (let [response (hndlr req)]
-                          (prometheus/with-path req response))))))))
+                          (with-path req response))))))))
