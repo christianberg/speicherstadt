eff69ba1751e1eec66d2397ec1a97e65502e4cfe
Christian Berg <berg.christian@gmail.com>
2016-03-20 18:30:07 +0100

    Update dependency: duct 0.5.10

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.stuartsierra/component "0.3.1"]
                  [compojure "1.5.0"]
-                 [duct "0.5.8"]
+                 [duct "0.5.10"]
                  [environ "1.0.2"]
                  [meta-merge "0.1.1"]
                  [me.raynes/fs "1.4.6"]
