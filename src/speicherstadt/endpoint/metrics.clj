f84d2f706d2ae939ee26355add868ee7abc2f1af
Christian Berg <berg.christian@gmail.com>
2017-01-06 15:16:37 +0100

    Remove code
    The system will be split up in Microservices, each in their own
    repo. The first such repo is speicherstadt-chunks-fs. This repo will
    only contain documentation and possibly code to orchestrate the
    services.

--- src/speicherstadt/endpoint/metrics.clj
+++
@@ -1,9 +0,0 @@
-(ns speicherstadt.endpoint.metrics
-  (:require [speicherstadt.component.metrics-store :as metrics-store]
-            [compojure.core :refer :all]))
-
-(defn metrics-endpoint [{:keys [metrics-store]}]
-  (routes
-   (context "/metrics" []
-            (GET "/" []
-                 (metrics-store/dump-metrics metrics-store)))))
