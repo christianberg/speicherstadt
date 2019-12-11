5b0d88fa6df491f543711d292d7df30f6072f746
Christian Berg <berg.christian@gmail.com>
2019-12-11 10:18:56 +0100

    Setup logging

--- services/chunks-fs/src/main.rs
+++ services/chunks-fs/src/main.rs
@@ -1,3 +1,18 @@
+#[macro_use]
+extern crate slog;
+extern crate slog_async;
+extern crate slog_term;
+
+use slog::Drain;
+
+fn root_logger() -> slog::Logger {
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    slog::Logger::root(drain, o!("service" => "chunks-fs"))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let logger = root_logger();
+    info!(logger, "Hello, world!");
 }
