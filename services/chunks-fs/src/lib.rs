189f13a06f90e035fbf86691b87493ecb971246a
Christian Berg <berg.christian@gmail.com>
2020-01-07 19:13:43 +0100

    Naive version of storing request body on disk

--- services/chunks-fs/src/lib.rs
+++ services/chunks-fs/src/lib.rs
@@ -22,13 +22,45 @@ pub fn start_server(port: u16, base_dir: std::path::PathBuf, parent_logger: &Log
     );
     router.put(
         "/chunks/sha256/:hash",
-        move |_req: &mut Request| -> IronResult<Response> {
+        move |req: &mut Request| -> IronResult<Response> {
             info!(put_chunk_logger, "Handling put_chunk request");
-            Ok(Response::with(status::Created))
+            match store_chunk(
+                &base_dir,
+                req.extensions
+                    .get::<Router>()
+                    .unwrap()
+                    .find("hash")
+                    .unwrap(),
+                &mut req.body,
+                &put_chunk_logger,
+            ) {
+                Ok(_) => Ok(Response::with(status::Created)),
+                Err(e) => {
+                    error!(put_chunk_logger, "{}", e);
+                    Err(IronError::new(e, status::InternalServerError))
+                }
+            }
         },
         "put_chunk",
     );
     Iron::new(router)
         .http(("localhost", port))
         .expect("Unable to start server");
+}
+
+fn store_chunk(
+    base_dir: &std::path::PathBuf,
+    hash: &str,
+    content: &mut dyn std::io::Read,
+    logger: &Logger,
+) -> std::io::Result<()> {
+    debug!(logger, "Storing chunk {}", hash);
+    let mut path = base_dir.clone();
+    path.push("sha256");
+    path.push(hash);
+    debug!(logger, "Storage location: {:?}", path);
+    let file = &mut std::fs::File::create(path)?;
+    std::io::copy(content, file).map(|size| {
+        debug!(logger, "{} bytes written", size);
+    })
 }
