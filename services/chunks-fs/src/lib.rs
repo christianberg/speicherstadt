bf444d826e1e16ec5c4636d281cca9f7ea26bb4d
Christian Berg <berg.christian@gmail.com>
2020-01-07 16:34:51 +0100

    Add new (dummy) route for PUT chunk requests

--- services/chunks-fs/src/lib.rs
+++ services/chunks-fs/src/lib.rs
@@ -9,15 +9,24 @@ use router::Router;
 use slog::Logger;
 
 pub fn start_server(port: u16, parent_logger: &Logger) {
-    let logger = parent_logger.new(o!("foo" => "bar"));
+    let hello_logger = parent_logger.new(o!("route" => "hello"));
+    let put_chunk_logger = parent_logger.new(o!("route" => "put_chunk"));
     let mut router = Router::new();
     router.get(
         "/",
         move |_req: &mut Request| -> IronResult<Response> {
-            info!(logger, "Handled hello request");
+            info!(hello_logger, "Handling hello request");
             Ok(Response::with((status::Ok, "Hello world!")))
         },
         "hello",
+    );
+    router.put(
+        "/chunks/sha256/:hash",
+        move |_req: &mut Request| -> IronResult<Response> {
+            info!(put_chunk_logger, "Handling put_chunk request");
+            Ok(Response::with(status::Created))
+        },
+        "put_chunk",
     );
     Iron::new(router)
         .http(("localhost", port))
