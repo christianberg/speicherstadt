07d1bb6d498df5d94683f0a2d60e17f184c94b65
Christian Berg <berg.christian@gmail.com>
2020-01-10 00:53:48 +0100

    Inject storage struct via middleware
    Injecting the struct responsible for storage and retrieval of chunks
    on disk into each request via a middleware allows the same struct to
    be used by different routes.

--- services/chunks-fs/src/lib.rs
+++ services/chunks-fs/src/lib.rs
@@ -3,6 +3,7 @@ extern crate iron;
 #[macro_use]
 extern crate slog;
 
+use iron::middleware::BeforeMiddleware;
 use iron::prelude::*;
 use iron::status;
 use router::Router;
@@ -28,84 +29,68 @@ impl iron::Handler for HelloHandler {
     }
 }
 
-struct ChunkHandler {
-    storage: Storage,
-    logger: Logger,
-}
-
-impl ChunkHandler {
-    fn new(storage: Storage, parent_logger: &Logger) -> Self {
-        Self {
-            storage,
-            logger: parent_logger.new(o!("route"=>"chunks")),
-        }
-    }
-
-    fn handle_put(&self, req: &mut Request) -> IronResult<Response> {
-        info!(self.logger, "Handling PUT chunk request");
-        match self.storage.store_chunk(
-            req.extensions
-                .get::<Router>()
-                .unwrap()
-                .find("hash")
-                .unwrap(),
-            &mut req.body,
-        ) {
-            Ok(_) => Ok(Response::with(status::Created)),
-            Err(e) => {
-                error!(self.logger, "{}", e);
-                Err(IronError::new(e, status::InternalServerError))
-            }
-        }
-    }
-
-    fn handle_get(&self, req: &mut Request) -> IronResult<Response> {
-        info!(self.logger, "Handling GET chunk request");
-        match self.storage.retrieve_chunk(
-            req.extensions
-                .get::<Router>()
-                .unwrap()
-                .find("hash")
-                .unwrap(),
-        ) {
-            Ok(file) => Ok(Response::with((status::Ok, file))),
-            Err(e) => {
-                error!(self.logger, "{}", e);
-                Err(IronError::new(e, status::InternalServerError))
-            }
-        }
+fn handle_put(req: &mut Request) -> IronResult<Response> {
+    let storage = req.extensions.get::<Storage>().unwrap();
+    match storage.store_chunk(
+        req.extensions
+            .get::<Router>()
+            .unwrap()
+            .find("hash")
+            .unwrap(),
+        &mut req.body,
+    ) {
+        Ok(_) => Ok(Response::with(status::Created)),
+        Err(e) => Err(IronError::new(e, status::InternalServerError)),
     }
 }
 
-impl iron::Handler for ChunkHandler {
-    fn handle(&self, req: &mut Request) -> IronResult<Response> {
-        info!(self.logger, "Handling {} request", req.method);
-        match &req.method {
-            iron::method::Method::Get => self.handle_get(req),
-            iron::method::Method::Put => self.handle_put(req),
-            _ => Ok(Response::with(status::MethodNotAllowed)),
-        }
+fn handle_get(req: &mut Request) -> IronResult<Response> {
+    let storage = req.extensions.get::<Storage>().unwrap();
+    match storage.retrieve_chunk(
+        req.extensions
+            .get::<Router>()
+            .unwrap()
+            .find("hash")
+            .unwrap(),
+    ) {
+        Ok(file) => Ok(Response::with((status::Ok, file))),
+        Err(e) => Err(IronError::new(e, status::InternalServerError)),
     }
 }
 
 struct Storage {
     data_dir: PathBuf,
-    logger: Logger,
+}
+
+impl iron::typemap::Key for Storage {
+    type Value = ChunkStorer;
+}
+
+impl BeforeMiddleware for Storage {
+    fn before(&self, req: &mut Request) -> IronResult<()> {
+        req.extensions.insert::<Storage>(ChunkStorer {
+            data_dir: self.data_dir.clone(),
+        });
+        Ok(())
+    }
 }
 
 impl Storage {
-    fn new(base_dir: PathBuf, parent_logger: &Logger) -> Self {
+    fn new(base_dir: PathBuf) -> Self {
         let mut data_dir = base_dir.clone();
         data_dir.push("data");
         std::fs::create_dir(&data_dir).unwrap();
         data_dir.push("sha256");
         std::fs::create_dir(&data_dir).unwrap();
-        Self {
-            data_dir,
-            logger: parent_logger.new(o!("component"=>"storage")),
-        }
+        Self { data_dir }
     }
+}
 
+struct ChunkStorer {
+    data_dir: PathBuf,
+}
+
+impl ChunkStorer {
     fn path_for_hash(&self, hash: &str) -> PathBuf {
         let mut path = self.data_dir.clone();
         path.push(hash);
@@ -113,12 +98,12 @@ impl Storage {
     }
 
     fn store_chunk(&self, hash: &str, content: &mut dyn std::io::Read) -> std::io::Result<()> {
-        debug!(self.logger, "Storing chunk {}", hash);
+        //        debug!(self.logger, "Storing chunk {}", hash);
         let path = self.path_for_hash(hash);
-        debug!(self.logger, "Storage location: {:?}", path);
+        //        debug!(self.logger, "Storage location: {:?}", path);
         let file = &mut std::fs::File::create(path)?;
         std::io::copy(content, file).map(|size| {
-            debug!(self.logger, "{} bytes written", size);
+            //            debug!(self.logger, "{} bytes written", size);
         })
     }
 
@@ -130,14 +115,13 @@ impl Storage {
 
 pub fn start_server(port: u16, base_dir: PathBuf, logger: &Logger) {
     let mut router = Router::new();
-    let storage = Storage::new(base_dir, logger);
+    let storage = Storage::new(base_dir);
     router.get("/", HelloHandler::new(logger), "hello");
-    router.any(
-        "/chunks/sha256/:hash",
-        ChunkHandler::new(storage, logger),
-        "chunks",
-    );
-    Iron::new(router)
+    router.get("/chunks/sha256/:hash", handle_get, "chunks_get");
+    router.put("/chunks/sha256/:hash", handle_put, "chunks_put");
+    let mut chain = Chain::new(router);
+    chain.link_before(storage);
+    Iron::new(chain)
         .http(("localhost", port))
         .expect("Unable to start server");
 }
